21f0ea94e5f5853728c0f7a045886cd23e6bfe00
edgarsilva <edgar.osc@gmail.com>
2015-03-05 16:49:49 -0600

    adds topic prefix example

---
+++ examples/prefix_topic/blink-robot.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var Cylon = require('cylon');
+
+Cylon.robot({
+  name: 'cybot',
+
+  connections: {
+    arduino: { adaptor: 'firmata', port: '/dev/ttyACM0' }
+  },
+
+  devices: {
+    led: { driver: 'led', pin: 13 }
+  },
+
+  work: function() {
+    // Add your robot code here,
+    // for this simple blink example
+    // we'll interacting with the
+    // robot through the 'led' device.
+  }
+});
+
+// ensure you install the API plugin first:
+// $ npm install cylon-api-socket-io
+Cylon.api(
+  'mqtt',
+  {
+  broker: 'mqtt://test.mosquitto.org',
+  prefix: 'cybot', // Optional
+});
+
+Cylon.start();
